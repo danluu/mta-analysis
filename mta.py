bed379870df91a5ee9a782cf03bc4b153a2682e7
Lee Haugen <haugenlee@leegenes.local>
2017-06-22 13:16:47 -0400

    get station mappings for 1 train

--- mta.py
+++ mta.py
@@ -1,5 +1,5 @@
 import gtfs_realtime_pb2, nyct_subway_pb2
-import requests
+import json
 
 # Stops with 1 & 2 & 3: Chambers, 14th, 34th, 42nd, 96th.
 # Stop id: 635 R20S L03S D13S A31S is 14th (635N/635S)
@@ -7,7 +7,7 @@ import requests
 
 # TODO: make this less... bad.
 # TODO: note that this doesn't check for the station.
-def is_1_train_at_14th_south():
+def is_1_train_at_14th_south(item):
     if not item.HasField('trip_update'):
         return False
     if not item.trip_update.HasField('trip'):
@@ -16,13 +16,26 @@ def is_1_train_at_14th_south():
         return False
     return True
 
+def get_stations():
+    with open('mta-files/stations.json') as s:
+        stations = json.load(s)
+    id_to_station = {}
+    for station in stations['result']:
+        id_to_station.update({station['id']: station['name']})
+    return id_to_station
 
 feed = gtfs_realtime_pb2.FeedMessage()
 with open('./mta-files/gtfs-2014-09-17-23-41','rb') as f:
     feed.ParseFromString(f.read())
-    for item in feed.entity:
-        if is_1_train_at_14th_south():
-            print(item)
+station_ids = set()
+for entity in feed.entity:
+    if is_1_train_at_14th_south(entity):
+        for stop_time in entity.trip_update.stop_time_update:
+            station_ids.add(stop_time.stop_id)
+id_to_station = get_stations()
+for station_id in station_ids:
+    print(station_id, id_to_station[station_id])
+
 
 # entity.trip_update.stop_time_update.stop_id
 #  Want this to be 635S
