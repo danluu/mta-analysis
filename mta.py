c8b91aea751e7921da12a1e12a942d94c491bded
Dan Luu <danluu@gmail.com>
2017-06-22 15:07:19 -0400

    Refactor feed scaffolding into fn.

--- mta.py
+++ mta.py
@@ -7,7 +7,7 @@ import json
 
 # TODO: make this less... bad.
 # TODO: note that this doesn't check for the station.
-def is_1_train_at_14th_south(item):
+def is_1_train(item):
     if not item.HasField('trip_update'):
         return False
     if not item.trip_update.HasField('trip'):
@@ -24,21 +24,41 @@ def get_stations():
         id_to_station.update({station['id']: station['name']})
     return id_to_station
 
-feed = gtfs_realtime_pb2.FeedMessage()
-with open('./mta-files/gtfs-2014-09-17-23-41','rb') as f:
-    feed.ParseFromString(f.read())
-station_ids = set()
-for entity in feed.entity:
-    if is_1_train_at_14th_south(entity):
-        for stop_time in entity.trip_update.stop_time_update:
-            station_ids.add(stop_time.stop_id)
-id_to_station = get_stations()
-for station_id in station_ids:
-    print(station_id, id_to_station[station_id])
+def readable_gtfs_dump(filename):
+    feed = get_feed(filename)
+    for entity in feed.entity:
+        if is_1_train(entity):
+            print(entity)
 
+def get_all_stations():
+    feed = get_feed('./mta-files/gtfs-2014-09-17-23-41')
+    station_ids = set()
+    for entity in feed.entity:
+        if is_1_train(entity):
+            for stop_time in entity.trip_update.stop_time_update:
+                station_ids.add(stop_time.stop_id)
+    id_to_station = get_stations()
+    for station_id in station_ids:
+        print(station_id, id_to_station[station_id])
+
+
+def get_feed(filename):
+    feed = gtfs_realtime_pb2.FeedMessage()
+    with open(filename,'rb') as f:
+        feed.ParseFromString(f.read())
+    return feed
 
 # entity.trip_update.stop_time_update.stop_id
 #  Want this to be 635S
 
 # entity.trip_update.trip.route_id
 #  Want this to be "1", "2", or "3". Let's say "1".
+
+# readable_gtfs_dump('./mta-files/gtfs-2014-09-17-23-46')
+# get_all_stations()
+
+hacky_predefined_filenames = ['./mta-files/gtfs-2014-09-17-23-41', './mta-files/gtfs-2014-09-17-23-46']
+
+# for filename in hacky_predefined_filenames:
+#     with open(filename)
+#     print(filename)
